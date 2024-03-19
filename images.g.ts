3cdd568f94a3fd45b778110de3aacfbd53841d8f
spoilmilked <156138163+spoilmilked@users.noreply.github.com>
2024-03-18 21:38:57 -0500

    ammo and fixing game logic

--- images.g.ts
+++ images.g.ts
@@ -41,25 +41,6 @@ e e e e e e e e e e e e e e e e
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
-            case "image3":
-            case "blob":return img`
-. . . . . . . . . . b 5 b . . . 
-. . . . . . . . . b 5 b . . . . 
-. . . . . . b b b b b b . . . . 
-. . . . . b b 5 5 5 5 5 b . . . 
-. . . . b b 5 d 1 f 5 5 d f . . 
-. . . . b 5 5 1 f f 5 d 4 c . . 
-. . . . b 5 5 d f b d d 4 4 . . 
-. b b b d 5 5 5 5 5 4 4 4 4 4 b 
-b d d d b b d 5 5 4 4 4 4 4 b . 
-b b d 5 5 5 b 5 5 5 5 5 5 b . . 
-c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
-c b d c d 5 5 b 5 5 5 5 5 5 b . 
-. c d d c c b d 5 5 5 5 5 d b . 
-. . c b d d d d d 5 5 5 b b . . 
-. . . c c c c c c c c b b . . . 
-. . . . . . . . . . . . . . . . 
-`;
             case "image4":
             case "blank":return img`
 . . . . . . . . . . . . . . . . 
@@ -98,13 +79,67 @@ c b d c d 5 5 b 5 5 5 5 5 5 b .
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
+            case "image3":
+            case "blob":return img`
+. f f f . f f f f . f f f . 
+f f f f f c c c c f f f f f 
+f f f f b c c c c b f f f f 
+f f f c 3 c c c c 3 c f f f 
+. f 3 3 c c c c c c 3 3 f . 
+. f c c c c 4 4 c c c c f . 
+. f f c c 4 4 4 4 c c f f . 
+. f f f b f 4 4 f b f f f . 
+. f f 4 1 f d d f 1 4 f f . 
+. . f f d d d d d d f f . . 
+. . e f e 4 4 4 4 e f e . . 
+. e 4 f b 3 3 3 3 b f 4 e . 
+. 4 d f 3 3 3 3 3 3 c d 4 . 
+. 4 4 f 6 6 6 6 6 6 f 4 4 . 
+. . . . f f f f f f . . . . 
+. . . . f f . . f f . . . . 
+`;
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "reload":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 1 1 1 1 1 1 . . . . 
+. . . . . . . . . . . 1 1 1 . . 
+. . . . . . . . . . . . . 1 . . 
+. . . . . . . . . . . . . 1 1 . 
+. . . . . . . . . . . . . . 1 . 
+. . 1 . . . . . . . . . . . . . 
+. . 1 . . . . . . . . . . . . . 
+. . 1 . . . . . . . . . . . . . 
+. . 1 . . . . . . . . . . . . . 
+. . 1 1 . . . . . . . . . . . . 
+. . . 1 1 1 . . . . . . . . . . 
+. . . . . 1 1 1 1 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 namespace SpriteKind {
     export const Block = SpriteKind.create()
 }
+namespace StatusBarKind {
+    export const Ammo = StatusBarKind.create()
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`FLOOR0`, function (sprite, location) {
     location1 = tiles.getTileLocation(11, 3)
     if (controller.B.isPressed()) {
@@ -21,7 +24,18 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     DirectionTracker = 1
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    shooting()
+    if (AMMObar.value == 0) {
+        if (100 <= info.score()) {
+            AMMObar.value += 100
+            info.changeScoreBy(-100)
+            game.splash("reload complete -100")
+        } else {
+            game.splash("Not enough AMMO")
+        }
+    }
+    if (AMMObar.value != 0) {
+        shooting()
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     DirectionTracker = 3
@@ -48,26 +62,39 @@ scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     DirectionTracker = 4
 })
+function Ammo2 () {
+    AMMObar = statusbars.create(20, 4, StatusBarKind.Ammo)
+    AMMObar.setLabel("AMMO")
+    AMMObar.attachToSprite(mySprite)
+    AMMObar.value = 100
+}
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    sprites.destroy(sprite)
+})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     DirectionTracker = 2
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`FLOOR`, function (sprite, location) {
     location1 = tiles.getTileLocation(4, 3)
     if (controller.B.isPressed()) {
-        tiles.setWallAt(location1, true)
-        mySprite.sayText("repairing...", 200, true)
-        if (counter == 1) {
-            tiles.setTileAt(location1, assets.tile`baracade`)
-        } else if (counter == 2) {
-            tiles.setTileAt(location1, assets.tile`baracade0`)
-            counter = 1
-        } else if (counter == 3) {
-            tiles.setTileAt(location1, assets.tile`baracade1`)
-            counter = 2
+        if (true) {
+            tiles.setWallAt(location1, true)
+            mySprite.sayText("repairing...", 200, true)
+            if (counter == 1) {
+                tiles.setTileAt(location1, assets.tile`baracade`)
+            } else if (counter == 2) {
+                tiles.setTileAt(location1, assets.tile`baracade0`)
+                counter = 1
+            } else if (counter == 3) {
+                tiles.setTileAt(location1, assets.tile`baracade1`)
+                counter = 2
+            }
         }
     }
 })
 function shooting () {
+    AMMObar.value += -10
     if (DirectionTracker == 1) {
         projectile = sprites.createProjectileFromSprite(assets.image`blade`, mySprite, 0, -31)
     } else if (DirectionTracker == 2) {
@@ -79,16 +106,18 @@ function shooting () {
     }
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    statusbar.value += -20
     if (0 == statusbar.value) {
         sprites.destroy(otherSprite)
         sprites.destroy(sprite)
+        info.player1.changeScoreBy(100)
     }
     sprites.destroy(projectile)
+    statusbar.value += -20
 })
 let zombie: Sprite = null
 let statusbar: StatusBarSprite = null
 let projectile: Sprite = null
+let AMMObar: StatusBarSprite = null
 let DirectionTracker = 0
 let location1: tiles.Location = null
 let counter = 0
@@ -96,10 +125,13 @@ let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`testing`)
 mySprite = sprites.create(assets.image`blob`, SpriteKind.Player)
 controller.moveSprite(mySprite)
-let SpawnTimer = 9000
 scene.cameraFollowSprite(mySprite)
 counter = 0
-game.onUpdateInterval(SpawnTimer, function () {
+Ammo2()
+info.setLife(3)
+game.splash("Press A to shoot B to repair baracade")
+info.setScore(100)
+game.onUpdateInterval(2000, function () {
     statusbar = statusbars.create(18, 2, StatusBarKind.Health)
     zombie = sprites.create(assets.image`myImage0`, SpriteKind.Enemy)
     zombie.follow(mySprite, 5)
